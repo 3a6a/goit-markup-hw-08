245a17b15442c491a3e827ac0a73c89c9fc054d1
Andrey_Zabelin <3a6a86@gmail.com>
2020-11-18 20:35:25 +0200

    changes

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $('.menu__btn').on('click', function(e) {
+//     e.preventDefault;
+//     $(this).toggleClass('menu__btn--active');
+//   });
+
+(function (){
+    const menu__btn = document.querySelector(`.menu__btn`);
+    const main__section = document.querySelector(`.main__section`);
+    const mobile__menu = document.querySelector(`.mobile__menu`);
+    menu__btn.addEventListener(`click`, () => {
+    menu__btn.classList.toggle(`menu__btn--active`);
+    main__section.classList.toggle(`menu__section--hiden`);
+    mobile__menu.classList.toggle(`mobile__menu--active`);
+    });
+}());
